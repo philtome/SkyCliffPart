4fccc950bfc26f0551dc6b50eeb431d3d7860b5f
Philip Tome <phil@tome.net>
2023-06-18 15:39:30 -0600

    added twig and VISITS does a basic twig call.  I need to put in a base and a simple template and start builing out

--- src/Controller/VisitController.php
+++ src/Controller/VisitController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Loader\ArrayLoader;
 
 class VisitController
 {
@@ -14,7 +15,14 @@ class VisitController
 
     #[Route('/visits')]
     public function visits (): Response {
-        return new Response('This is the visits page');
+
+
+        $loader = new ArrayLoader([
+            'index' => 'Hello {{ name }}!',
+        ]);
+        $twig = new \Twig\Environment($loader);
+        return new Response($twig->render('index', ['name' => 'Fabien']));
+
     }
 
     #[Route('/contacts/{slug}')]
